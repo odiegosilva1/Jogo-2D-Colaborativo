23606ba825b9d3112c1afec35a1cbaf9ca67ca4c
odiegosilva1 <odiegosilvadev@gmail.com>
2025-04-01 10:19:30 -0300

    create: menu

---
+++ a-ilha-2d/scripts/menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _ready() -> void:
+	for button in get_tree().get_nodes_in_group("button"):
+		# Conexão correta no Godot 4 (sem argumentos extras)
+		button.pressed.connect(_on_button_pressed.bind(button))
+
+func _on_button_pressed(button: Button) -> void:
+	match button.name:
+		"jogar":
+			var _game: bool = get_tree().change_scene_to_file("res://scenes/main.tscn")  # Godot 4 usa change_scene_to_file()
+		"continue":
+			print("Continuar jogo")
+		"sair":
+			get_tree().quit()
